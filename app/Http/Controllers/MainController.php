af416057f6a48bbe9861ed4f8320c22e2f443c1d
TheArdent <oleksii.yaryi@gmail.com>
2017-02-05 14:29:07 +0200

    add at construct menu and view nav bar

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Menu;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function __contruct()
+    public function __construct(Menu $menu)
     {
     	$this->data = [];//All Controllers extends MainController
-	    //TODO add to $this->data nav bar
+	    $this->data['menu'] = $menu->getMenu();
     }
 }
