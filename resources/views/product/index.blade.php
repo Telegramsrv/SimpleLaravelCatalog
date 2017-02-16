f565f1b7e2a31b040938cf102e05c13cf736247d
TheArdent <oleksii.yaryi@gmail.com>
2017-02-16 19:09:44 +0200

    update view  for single product and list product

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,23 +4,31 @@
    Product page
 @endsection
 
+@section('gallery')
+    <div class="ism-slider" id="my-slider" style="padding-top: 60%">
+        <ol>
+            @foreach( $product->getGalery() as $slider)
+                <li>
+                    <img src="/{{$slider->image}}">
+                </li>
+            @endforeach
+        </ol>
+    </div>
+@endsection
+
 @section('content')
 
     <div class="col-md-4">
-        <img src="/{{$product->image}}" />
-
-        @foreach($product->getGalery() as $galery)
-            <img src="/{{$galery->image}}">
-        @endforeach
-
+        <img class="img-rounded" width="100%" src="/{{$product->image}}" /><br/><br/>
+        @yield('gallery')
     </div>
     <div class="col-md-8">
         <h2>{{$product->name}}</h2>
         <p>{!! $product->description !!}</p>
     </div>
-
-    <div class="col-md-8">
-
+    <hr class="featurette-divider">
+    <div class="col-md-12">
+        <h3>Reviews</h3>
         @foreach($product->getReview() as $review)
             <div class="col-md-8">
                 <h3> {{$review->user_name}} <small>{{$review->email}}</small></h3>
@@ -28,6 +36,7 @@
                 <strong>Star:{{$review->star}}</strong><br/>
                 <small>{{date('H:m:s d-m-Y',$review->created_at)}}</small>
             </div>
+            <hr class="featurette-divider">
         @endforeach
 
     </div>
--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -6,15 +6,14 @@
 
 @section('content')
 
-    @foreach($products as $item)
-
-            <div class="col-md-3">
-                <a href="{{ route('product.slug',['slug' => $item->slug]) }}">
-                    <img src="/{{$item->image}}">
-                    <p>{{$item->name}}</p>
-                </a>
+    <div class="container">
+        @foreach( $products as $product)
+            <div class="col-lg-4">
+                <a href="{{ route('product.slug',['slug' => $product->slug]) }}">
+                    <img class="img-rounded" src="/{{$product->image}}" alt="{{$product->name}}" width="140" height="120">
+                    <h2>{{$product->name}}</h2></a>
             </div>
-
-    @endforeach
+        @endforeach
     {!! $products->render() !!}
+    </div>
 @stop
